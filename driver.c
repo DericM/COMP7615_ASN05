04314f1593ae3eda04e1f40e9385f75063df465a
DericM <dmccadden@my.bcit.ca>
2018-11-05 21:28:39 -0800

    init

--- driver.c
+++ driver.c
@@ -2,8 +2,10 @@
   2018-09-30*/
 #include <stdio.h>
 
-extern int asm_main(void);
+extern int mult_asm(int, int);
 
 int main(){
-    asm_main();
+    int c;
+    c = mult_asm(3000000, -4000000);
+    printf("Result is %d \n", c);
 }
